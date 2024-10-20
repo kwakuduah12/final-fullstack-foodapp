a79f2697ea2358c8f27b18ad6cec02a32ee9e964
Philip De-Graft <115908616+kwakuduah12@users.noreply.github.com>
2024-10-20 18:32:12 -0400

    iOS app code (#6)

---
+++ HomeDash/HomeDashApp.swift
@@ -0,0 +1,18 @@
+//
+//  HomeDashApp.swift
+//  HomeDash
+//
+//  Created by Kwaku Duah De-Graft on 10/2/24.
+//
+
+import SwiftUI
+
+@main
+struct HomeDashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            AuthView()
+//            ContentView()
+        }
+    }
+}
