18e525fcecbb32a0a9811482cf9055d6071f0d69
Vincent <vsalihu@stetson.edu>
2024-10-20 18:04:32 -0400

    server and db setup

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 require("dotenv").config();
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-console.log('DB_URI:', process.env.DB_URI);
+//console.log('DB_URI:', process.env.DB_URI);
 
 const port = process.env.PORT || 5000
 const uri = process.env.DB_URI;
 
 const app = express();
+const UserRoutes = require('./routes/userRoutes');
+
+app.use(cors());
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+
+app.use('/user', UserRoutes);
 const connectDB = async () => {
     try {
         await mongoose.connect(uri);
@@ -21,7 +28,6 @@ const connectDB = async () => {
         process.exit(1);
     }
 }
-
 connectDB();
 
 
@@ -30,14 +36,8 @@ app.get('/', function(req, res) {
     res.send('Hello World');
 });
 
-app.get('/hello/:name', function(req, res) {
-    res.send('Hello ' + req.params.name);
-})
 
-app.post('/login', function(req, res) {
-    req.body
-});
 
-app.listen(port, () => {
+app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running on port ${port}`);
 });
