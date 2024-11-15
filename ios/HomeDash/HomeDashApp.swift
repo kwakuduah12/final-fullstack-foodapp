4938c7f1a66dc61c613cfba46174061c7a0e2857
Philip <ayebeamel@gmail.com>
2024-11-15 14:03:12 -0500

    changes to the ios app

--- ios/HomeDash/HomeDashApp.swift
+++ ios/HomeDash/HomeDashApp.swift
@@ -1,29 +1,15 @@
-//
 //  HomeDashApp.swift
 //  HomeDash
 //
 //  Created by Kwaku Duah De-Graft on 10/2/24.
-//
 
 import SwiftUI
 
 @main
-struct HomeDashApp: App {
-    @AppStorage("isAuthenticated") var isAuthenticated: Bool = false
-    @AppStorage("userRole") var userRole: String = "User" // Store user role ("User" or "Merchant")
-
+struct YourAppNameApp: App {
     var body: some Scene {
         WindowGroup {
-            if isAuthenticated {
-                if userRole == "Merchant" {
-                    MerchantView() // Display MerchantView if the user is a merchant
-                } else {
-                    ContentView() // Display ContentView for regular users
-                }
-            } else {
-                AuthView()
-            }
+            RootView() // Start with RootView
         }
     }
 }
-
